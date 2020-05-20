beb63d9622ddbaf85db3c03645bcf0d24b033813
Jun <lhcjun@gmail.com>
2020-05-20 14:22:13 +0800

    remove Redux files

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-deal with GraphQL
+Based on [original project](https://github.com/lhcjun/E-commerce-CrownClothing), but built with GraphQL + Apollo and removed Redux + Redux-Saga
 
-package
-apollo-boost
-react-apollo
-graphql
-apollo-cache-persist
+## package used
+-----------------
+    * apollo-boost<br>
+    * react-apollo<br>
+    * graphql<br>
+    * apollo-cache-persist
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-boost";
 import { persistCache } from "apollo-cache-persist";
 
-import { store, persistor } from "./redux/store";
-import { default as data } from './graphql/initial-data';
-import { default as App } from './containers/App.container';
+import { default as data } from "./graphql/initial-data";
+import { default as App } from "./containers/App.container";
 import { resolvers, typeDefs } from "./graphql/resolver";
 import "./index.css";
 
@@ -40,13 +37,9 @@ client.writeData({ data });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
   document.getElementById("root")
 );
